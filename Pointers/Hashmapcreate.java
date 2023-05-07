84d3684479b51c7e00837355ccd7b94a35586b1f
DIVYANSH SINGHAL <007singhaldivyansh@gmail.com>
2023-05-07 16:04:55 +0530

    Added 3rd map

--- Pointers/Hashmapcreate.java
+++ Pointers/Hashmapcreate.java
@@ -13,5 +13,13 @@ public class Hashmapcreate{
         map.put("Value",2);
         System.out.println(map);
         System.out.println(map1);
+        //introducing third map
+        HashMap<String, Integer>map2=new HashMap<>(map);
+        map2.put("value",3);
+        map1=map2;
+        map=map2;
+        System.out.println(map);
+        System.out.println(map1);
+        System.out.println(map2);   
     }
 }
