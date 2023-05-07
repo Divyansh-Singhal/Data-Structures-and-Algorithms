1ca6d2191117c107c0477e1ffe0f0e7877ca21f6
DIVYANSH SINGHAL <007singhaldivyansh@gmail.com>
2023-05-07 22:10:53 +0530

    Added append

---
+++ LinkedList/Append/LinkedList.java
@@ -0,0 +1,39 @@
+package LinkedList.Append;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value=value;
+        }
+    }
+
+    public LinkedList(int value){
+        Node newnode=new Node(value);
+        head=newnode;
+        tail=newnode;
+        length=1;
+    }
+
+    public void printall(){
+        if(length==0){
+            System.out.println("Empty List");
+        }
+        else{
+            System.out.println(head.value);
+            System.out.println(tail.value);
+        }
+        System.out.println(length);
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.value+" ");
+            temp=temp.next;
+        }
+    }
+}
