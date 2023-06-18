41ca7b2e7a327798e312cd73ef5f092618ff771f
DIVYANSH SINGHAL <007singhaldivyansh@gmail.com>
2023-06-18 17:28:59 +0530

    Added Prepend

---
+++ LinkedList/Prepend/LinkedList.java
@@ -0,0 +1,66 @@
+package LinkedList.Prepend;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value=value;
+        }
+    }
+
+    public void append(int value){
+        Node newnode=new Node(value);
+        if(length==0){
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            tail.next=newnode;
+            tail=newnode;
+        }
+        length++;
+    }
+
+    public void prepend(int value){
+        Node newnode=new Node(value);
+        if(length==0){
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            newnode.next=head;
+            head=newnode;
+        }
+        length++;
+    }
+
+    public LinkedList(int value){
+        Node newnode=new Node(value);
+        head=newnode;
+        tail=newnode;
+        length=1;
+    }
+
+    public void printall(){
+        if(length==0){
+            System.out.println("Empty List");
+        }
+        else{
+            System.out.println(head.value);
+            System.out.println(tail.value);
+        }
+        System.out.println(length);
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.value+" ");
+            temp=temp.next;
+        }
+    }
+}
+
